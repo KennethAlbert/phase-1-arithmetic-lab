e9d28a083f0ae13556c4b3def60b25ea7de2a6c1
Kenneth Albert <gichuka@gmail.com>
2022-06-08 23:44:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+
+const multiply= num1*num2;
+
+const random=parseInt(Math.random() + 1);
+
+let num3=16;
+let num4=6;
+
+const mod=num3%num4;
+
+const max=Math.max(1, 2, 3, 4, 20)
+
